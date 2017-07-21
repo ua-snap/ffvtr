7207662fa46d6536d61c65492d93a5824bf9c23c
Bruce Crevensten <bruce.crevensten@gmail.com>
2017-07-21 13:54:18 -0700

    add linear interpolation & rounding

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 library(shiny)
 currentYear <- 2016
 currentStudentFte <- 19229
-fteYears <- c(2016, 2018:2020, 2025)
+fteYears <- c(2016, 2018:2025)
 
 shinyServer(function(input, output, session) {
 
@@ -20,26 +20,42 @@ shinyServer(function(input, output, session) {
     exponentialGrowth <- function(year, baseFte, percentage) {
     decimalGrowth <- 1 + percentage / 100
     yearOffset <- year - currentYear
-    return(baseFte * decimalGrowth ^ yearOffset)
+    return(round(baseFte * decimalGrowth ^ yearOffset))
   }
 
   studentFteGrowth <- reactive({ sapply(fteYears, exponentialGrowth, baseFte=currentStudentFte, percentage=input$studentFtePercentChange) })
   tuitionFeesFTE <- reactive(
-    c(
-      6806, # 2016
-      input$tuitionFeesFTE2018,
-      input$tuitionFeesFTE2019,
-      input$tuitionFeesFTE2020,
-      input$tuitionFeesFTE2025
+    sapply(
+      c(
+        6806, # 2016
+        input$tuitionFeesFTE2018,
+        input$tuitionFeesFTE2019,
+        input$tuitionFeesFTE2020,
+        approx(
+          c(2020, 2025),
+          c(input$tuitionFeesFTE2020, input$tuitionFeesFTE2025),
+          c(2021:2024)
+        )$y,
+        input$tuitionFeesFTE2025
+      ),
+      round
     )
   )
   totalStateAppropriation <- reactive(
-    c(
-      350, # 2016
-      input$totalStateAppropriation2018,
-      input$totalStateAppropriation2019,
-      input$totalStateAppropriation2020,
-      input$totalStateAppropriation2025
+    sapply(
+      c(
+        350, # 2016
+        input$totalStateAppropriation2018,
+        input$totalStateAppropriation2019,
+        input$totalStateAppropriation2020,
+        approx(
+          c(2020, 2025),
+          c(input$totalStateAppropriation2020, input$totalStateAppropriation2025),
+          c(2021:2024)
+        )$y,
+        input$totalStateAppropriation2025
+      ),
+      round
     )
   )
   output$studentFtes <- renderPrint({ studentFteGrowth() })
