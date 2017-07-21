f754c4368bae64da99ec1673164c2fb32fe5e0a6
afloyd8 <afloyd8@alaska.edu>
2017-07-20 17:50:00 -0800

    Changed layout to fluidPage, using fluidRow and column to rearrange UI. Added global.R script for lists and input arguments used to create the sliders and also stored the call to library(shiny) there. Added a function in ui.R to create rows and columns of numericInput/sliderInput combos. Synced the sliders and numericInput boxes, and used lapply() to shorten the slider code.

---
+++ global.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+slider.args1 <- list( # arguments for first row of sliders/numeric inputs
+  list(inputId='studentFtePercentChange', label="% change student FTEs per year", value=2, min=-5, max=5, step=1)
+)
+
+slider.args2 <- list( # arguments for first row of sliders/numeric inputs
+  list(inputId='tuitionFeesFTE2018', label='2018', value=7000, min=5000, max=15000, step=1),
+  list(inputId='tuitionFeesFTE2019', label='2019', value=7500, min=5000, max=15000, step=1),
+  list(inputId='tuitionFeesFTE2020', label='2020', value=8000, min=5000, max=15000, step=1),
+  list(inputId='tuitionFeesFTE2025', label='2025', value=10089, min=5000, max=15000, step=1)
+)
+
+slider.args3 <- list( # arguments for second row of sliders/numeric inputs
+  list(inputId='totalStateAppropriation2018', label='2018', value=340, min=200, max=500, step=1),
+  list(inputId='totalStateAppropriation2019', label='2019', value=335, min=200, max=500, step=1),
+  list(inputId='totalStateAppropriation2020', label='2020', value=333, min=200, max=500, step=1),
+  list(inputId='totalStateAppropriation2025', label='2025', value=312, min=200, max=500, step=1)
+)
--- ui.R
+++ ui.R
@@ -1,35 +1,31 @@
-library(shiny)
+
+make_inputs_row <- function(x, col=3, labels="top", suffix="slider"){
+  fluidRow(
+    lapply(seq_along(x), function(i, x){
+      x <- x[[i]]
+      if(labels=="none") x$label <- ""
+      numbox <- do.call(numericInput, c(x, width="100%"))
+      if(labels=="top") x$label <- ""
+      x$inputId <- paste0(x$inputId, suffix)
+      slider <- do.call(sliderInput, c(x, width="100%"))
+      column(col, div(numbox, style='height:50px;'), slider)
+    }, x=x)
+  )
+}
+
 
 shinyUI(fluidPage(
-  
   titlePanel("UA Financial Framework Visualization Tool"),
-  sidebarLayout(
-    
-    sidebarPanel(
-      sliderInput("studentFtePercentChange",
-                  label = "% change student FTEs per year", min = -5, max = 5, value = 2),
-      h4("Tuition and fees per student FTE ($)"),
-      sliderInput("tuitionFeesFTE2018",
-                  label = "2018", min = 5000, max = 15000, value = 7000),
-      sliderInput("tuitionFeesFTE2019",
-                  label = "2019", min = 5000, max = 15000, value = 7500),
-      sliderInput("tuitionFeesFTE2020",
-                  label = "2020", min = 5000, max = 15000, value = 8000),
-      sliderInput("tuitionFeesFTE2025",
-                  label = "2025", min = 5000, max = 15000, value = 10089),
-      h4("Total State Appropriation (Million $)"),
-      sliderInput("totalStateAppropriation2018",
-                  label = "2018", min = 200, max = 500, value = 340),
-      sliderInput("totalStateAppropriation2019",
-                  label = "2019", min = 200, max = 500, value = 335),
-      sliderInput("totalStateAppropriation2020",
-                  label = "2020", min = 200, max = 500, value = 333),
-      sliderInput("totalStateAppropriation2025",
-                  label = "2025", min = 200, max = 500, value = 312)
-    ),
-    
-    mainPanel(
-      "placeholder for spreadsheet and graphs"
+  fluidPage(
+    fluidRow(
+      column(6,
+             make_inputs_row(slider.args1, 6),
+             make_inputs_row(slider.args2),
+             make_inputs_row(slider.args3, labels='none')
+      ),
+      column(6,
+             "placeholder for spreadsheet and graphs"
+      )
     )
   )
 ))
