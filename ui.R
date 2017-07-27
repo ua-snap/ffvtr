2b334996bd8f12f05783464df61aa1abf28cb28e
Craig Stephenson <cstephen@iarc.uaf.edu>
2017-07-27 08:41:08 -0800

    Add dark Bootstrap navbar, move buttons into it, and make spreadsheet… (#20)
    * Add dark Bootstrap navbar, move buttons into it, and make spreadsheet table taller.
    
    * Line up spreadsheet header cell text with normal cell text.
    
    * Fix responsive resizing of navbar.
    
    * Increase graph axis and tick label text, fix up legend, and remove unnecessary legends.
    
    * Use 2016 data as placeholder data for 2017.

--- ui.R
+++ ui.R
@@ -13,30 +13,44 @@ make_inputs_row <- function(x, col=3, labels="top", suffix="slider"){
   )
 }
 
+page_title <- "UA Financial Framework Visualization Tool"
+
 function(request) {
-  fluidPage(
-    titlePanel("UA Financial Framework Visualization Tool"),
-    fluidPage(
-      fluidRow(
-        column(6,
-               h4("% change student FTEs per year"),
-               make_inputs_row(slider.args1, 6, labels = "none"),
-               h4("Tuition and Fees per Student FTE ($)"),
-               make_inputs_row(slider.args2),
-               h4("Total State Appropriation (Million $)"),
-               make_inputs_row(slider.args3),
-               actionButton("reset", label = "Reset Plots"),
-               bookmarkButton()
-        ),
-        column(6,
-               DT::dataTableOutput("spreadsheet")
-        )
+  navbarPage(
+    inverse = TRUE,
+    fluid = TRUE,
+    theme = "style.css",
+    windowTitle = page_title,
+    title = div(
+      class = "navbar-content",
+      span(
+        class = "navbar-title",
+        page_title
       ),
-      fluidRow(
-        column(4, plotOutput("enrollmentPlot")),
-        column(4, plotOutput("compositePlot")),
-        column(4, plotOutput("appropriationsPlot"))
+      span(
+        class = "pull-right",
+        actionButton("reset", label = "Reset Plots"),
+        bookmarkButton()
       )
+    ),
+    fluidRow(
+      column(6,
+        h4("% change student FTEs per year"),
+        make_inputs_row(slider.args1, 6, labels = "none"),
+        h4("Tuition and Fees per Student FTE ($)"),
+        make_inputs_row(slider.args2),
+        h4("Total State Appropriation (Million $)"),
+        make_inputs_row(slider.args3)
+      ),
+      column(6,
+        DT::dataTableOutput("spreadsheet")
+      )
+    ),
+    fluidRow(
+      class = "graph-row",
+      column(4, plotOutput("enrollmentPlot")),
+      column(4, plotOutput("compositePlot")),
+      column(4, plotOutput("appropriationsPlot"))
     )
   )
 }
